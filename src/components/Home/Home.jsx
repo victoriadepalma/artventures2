910d7274c85625f39ceb0562b9945e5c888a0cac
Victoria De Palma <victoriadepalma@Victorias-MacBook-Air.local>
2023-06-17 12:09:42 -0400

    Home details and Routes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Mision } from './Mision'
 import "./Home.css"
 import { Portada } from './Portada'
+import { Localidades } from './Localidades'
 
 export const Home = () => {
   return (
     <>
     <Portada/>
     <Mision/>
+    <Localidades/>
     </>
   )
 }
---
+++ src/components/Home/Localidades.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import biblioteca from "../imagenes/biblioteca.jpg"
+
+export const Localidades = () => {
+  return (
+    <div className='localidades'>
+        <div className='square'>
+        <img className="imagen" src = {biblioteca}></img>
+        </div>
+        <h1 className='titulo-localidades'>Localidades</h1>
+        <p className='info-l'>
+            Biblioteca Pedro Grases.
+            CELAUP.
+            Laboratorios de Computadoras de la BIblioteca.
+            Sala Humanística.
+            Recepción.
+            Jardines.</p>
+    </div>
+  )
+}
