df5bc4401ced8667e426f0c1626b71626befd77e
Victoria De Palma <victoriadepalma@Victorias-MacBook-Air.local>
2023-06-23 20:42:16 -0400

    Slide Tours

--- src/Views/Tours/Tour.jsx
+++ src/Views/Tours/Tour.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Carousel } from '../../components/Carousel/Carousel'
 
 export const Tour = ({item}) => {
   return (
     <>
     
     <div className='tour'>
+        <Carousel/>
     <h1 className='tour-name'>TITULO</h1>
     <Link to= "/book"> <button className='ver-mas-button'>Ver más</button> </Link>
         
@@ -13,8 +15,7 @@ export const Tour = ({item}) => {
     </div>
     
     </div>
-   
-   
   </>
   )
 }
+
--- src/Views/Tours/Tours.jsx
+++ src/Views/Tours/Tours.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Carousel } from '../../components/Carousel/Carousel'
 import { LogoN } from '../../components/Navbar/LogoN'
 import { Tour } from './Tour'
 import "./Tours.css"
 
 export const Tours = () => {
   return (
-    <>
+  <> 
   <LogoN/>
-   <Tour/>
-   <Tour/>
-   <Tour/>
-   </>
+    
+     <Tour/>
+     
+    
+
+
+  </>
   )
 }
