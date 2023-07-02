380bdb1d9dad3d6f1945b0028156336be6ca6aa2
Diana Silva <diana.silva@correo.unimet.edu.ve>
2023-07-02 03:11:26 -0400

    Tours Filters position fixed removed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ea2a69de'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9susab6ee18"
+    "revision": "0.41qenb48c28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
