3c5d83c6d70f1ba86cf64901ae1c71a85090a4d5
Diana Silva <diana.silva@correo.unimet.edu.ve>
2023-07-02 19:03:40 -0400

    Payment Validation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ea2a69de'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rg1ft0tdung"
+    "revision": "0.6g8rjbca18g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
