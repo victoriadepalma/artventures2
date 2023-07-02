7007a85ade1ccd132e13d5321253c15e05ad497b
Victoria De Palma <victoriadepalma@Victorias-MacBook-Air.local>
2023-07-02 19:26:29 -0400

    responsive fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ea2a69de'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jf6daree0fo"
+    "revision": "0.d6uc8q02nug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
