f221f01f288011f7589304d3f1e87fadf0f61fe0
Diana Silva <diana.silva@correo.unimet.edu.ve>
2023-07-02 19:15:45 -0400

    Merge Develop

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ea2a69de'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t7vm6as0f3o"
+    "revision": "0.padq0tbtsi8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
