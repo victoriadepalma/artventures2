2693186a32b559f1c5f1643fbe14b58c3c23773e
Diana Silva <diana.silva@correo.unimet.edu.ve>
2023-07-02 20:49:41 -0400

    Buttons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ea2a69de'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f5aejek9t9g"
+    "revision": "0.ak0fuk25tog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
